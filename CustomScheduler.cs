26f45500c976d7d5a47ebf4a96467241025cfa5c
ericpfeifer-izenda <eric.pfeifer@izenda.com>
2016-10-19 13:22:12 -0400

    Add files via upload

--- CustomScheduler.cs
+++ CustomScheduler.cs
@@ -1,165 +1,151 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
-using Izenda.AdHoc;
 using System.IO;
 using System.Net.Mail;
+using System.Text;
+using System.Web;
+using Izenda.AdHoc;
+using System.Net;
 
-namespace CustomScheduler {
-  class CustomScheduler {
-    static void Main(string[] args) {
-      Izenda.AdHoc.AdHocSettings.LicenseKey = "Insert Key Here";
-      Izenda.AdHoc.AdHocSettings.SqlServerConnectionString = @"Insert Connection String Here";
-      Izenda.AdHoc.AdHocSettings.AdHocConfig = new CustomAdHocConfig();
-      Izenda.AdHoc.AdHocSettings.ReportsPath = @"Path to reports folder";
-      Izenda.AdHoc.AdHocSettings.ShowPoweredByLogo = false;
-      AdHocSettings.SmtpServer = "Mail server";
-      AdHocSettings.EmailFromAddress = "Someone@domain.com";
-      AdHocSettings.ResponseServer = @"URl of rs.aspx";
-      RunScheduledReports();
-    }
+public class CustomScheduler 
+{
+	public static void RunScheduledReports(TextWriter log, DateTime dateTime) 
+	{
+		string[] tenants = ListTenants();
+		RunSchedulerForTenant(log, dateTime, null);
+		foreach (string tenant in tenants)
+			RunSchedulerForTenant(log, dateTime, tenant);
+	}
 
-    private static void RunScheduledReports() {
-      string delimiter = Environment.NewLine;
-      try {
-        bool reportsFound = false;
-        DateTime dateTime = DateTime.Now;
+	protected static void RunSchedulerForTenant(TextWriter log, DateTime dateTime, string tenantId) 
+	{
+		AdHocSettings.CurrentUserIsAdmin = true;
+		AdHocSettings.CurrentUserTenantId = tenantId;
+		if (System.Web.HttpContext.Current.Items.Contains("ListReportsWasCalledOnce"))
+			System.Web.HttpContext.Current.Items.Remove("ListReportsWasCalledOnce");
+		AdHocContext.SetSchedulerExecuting(true);
+		try 
+		{
+			string delimiter = HttpContext.Current == null ? Environment.NewLine : "<br/>";
+			bool reportsFound = false;
 
-        ReportInfo[] reports = AdHocSettings.AdHocConfig.ListReports();
-        foreach (ReportInfo reportInfo in reports) {
-          if (reportInfo.FullName == null || reportInfo.FullName == "") {
-            continue;
-          }
-          ReportSet reportSet = null;
-          try {
-            //reportSet = AdHocSettings.AdHocConfig.LoadFilteredReportSet(reportInfo.FullName);
-            reportSet = AdHocSettings.AdHocConfig.LoadReportSet(reportInfo.FullName);
-          } catch {
-          }
-          if (reportSet == null) {
-            continue;
-          }
+			// Send all emails using the same SmtpClient
+			SmtpClient client = new SmtpClient(AdHocSettings.SmtpServer);
 
-          DateTime schedule = reportSet.Schedule;
-          string reportName = reportInfo.Name;
-          if (reportInfo.Category != null && reportInfo.Category != "") {
-            reportName = reportInfo.Category + "\\" + reportInfo.Name;
-          }
-          reportSet.ReportName = reportName;
-          reportsFound = true;
+			ReportInfo[] reports = AdHocSettings.AdHocConfig.ListReports();
+			foreach (ReportInfo reportInfo in reports) 
+			{
+				if (string.IsNullOrEmpty(reportInfo.Name) || reportInfo.TenantID.ToUpperInvariant() != tenantId.ToUpperInvariant())
+					continue;
+				ReportSet reportSet = null;
+				try 
+				{
+					reportSet = AdHocSettings.AdHocConfig.LoadFilteredReportSet(reportInfo.FullName);
+				} 
+				catch { }
+				if (reportSet == null)
+					continue;
 
-          if (schedule == Utility.NullDateTime || reportSet.Recipients == "" || reportSet.Recipients == null) {
-            Console.WriteLine(string.Format("{1}{0} - No Schedule{1}", reportName, delimiter));
-            continue;
-          }
-          if (schedule.Year == DateTime.MaxValue.Year) {
-            Console.WriteLine(string.Format("{1}{0} - Scheduled For Past {1}", reportName, delimiter));
-            continue;
-          }
-          if (schedule > dateTime) {
-            Console.WriteLine(
-              string.Format("{1}{0} - Scheduled In Future({2} @ {3}){1}", reportName, delimiter, schedule.ToShortDateString(),
-                            schedule.ToShortTimeString()));
-            continue;
-          }
-          try {
-            Console.WriteLine(
-              string.Format("{1}{0} - Scheduled For This Interval ({2} @ {3}){1}", reportName, delimiter,
-                            schedule.ToShortDateString(), schedule.ToShortTimeString()));
+				DateTime schedule = reportSet.Schedule;
+				DateTime scheduleUtc = reportSet.ScheduleUtc;
+				string reportName = reportInfo.Name;
+				if (!string.IsNullOrEmpty(reportInfo.Category))
+					reportName = reportInfo.Category + AdHocSettings.CategoryCharacter + reportInfo.Name;
+				reportsFound = true;
+				if (scheduleUtc == Utility.NullDateTime || string.IsNullOrEmpty(reportSet.Recipients))
+				{
+					log.Write(string.Format("{1}{0} - No Schedule{1}", reportName, delimiter));
+					continue;
+				}
+				if (schedule.Year == DateTime.MaxValue.Year)
+				{
+					log.Write(string.Format("{1}{0} - Scheduled For Past {1}", reportName, delimiter));
+					continue;
+				}
+				if (scheduleUtc > dateTime.ToUniversalTime())
+				{
+					log.Write(string.Format("{1}{0} - Scheduled In Future({2} @ {3}){1}", reportName, delimiter, schedule.ToShortDateString(), schedule.ToShortTimeString()));
+					continue;
+				}
+				try 
+				{
+					log.Write(string.Format("{1}{0} - Scheduled For This Interval ({2} @ {3}){1}", reportName, delimiter, schedule.ToShortDateString(), schedule.ToShortTimeString()));
+					string[] recipients = reportSet.Recipients.Split(',', ';');
+					MailMessage message = null;
 
-            string[] recipients = reportSet.Recipients.Split(',', ';');
+					#region Preparing mail
 
+					if (recipients.Length > 0) 
+					{
+						DateTime nextScheduleUtc = Utility.GetNextTime(scheduleUtc, reportSet.RepeatType).DateTime;
+						DateTime nowUtc = DateTime.UtcNow;
+						while (nextScheduleUtc < nowUtc)
+							nextScheduleUtc = Utility.GetNextTime(nextScheduleUtc, reportSet.RepeatType).DateTime;
+						reportSet.ScheduleUtc = nextScheduleUtc;
+						AdHocSettings.AdHocConfig.SaveReportSet(reportInfo, reportSet);
 
-            if (recipients.Length > 0) {
-              DateTime nextSchedule = Utility.GetNextTime(schedule, reportSet.RepeatType).DateTime;
-              while (nextSchedule < DateTime.Now) {
-                nextSchedule = Utility.GetNextTime(DateTime.Now, reportSet.RepeatType).DateTime;
-                nextSchedule =
-                  new DateTime(nextSchedule.Year, nextSchedule.Month, nextSchedule.Day,
-                               schedule.Hour, schedule.Minute, 0);
-              }
-              reportSet.Schedule = nextSchedule;
-              AdHocSettings.AdHocConfig.SaveReportSet(
-                reportInfo,
-                reportSet);
-            }
+						log.Write("Preparing mail - ");
+						if (!Utility.CheckCondition(reportSet))
+						{
+							log.WriteLine("Condition is false" + delimiter);
+							continue;
+						}
+						SchedulerOutput sh = AdHocSettings.SchedulerOutputTypes[reportSet.SendEmailAs];
 
+						message = sh.GenerateMessage(reportSet, reportSet.IsDashBoard);
 
-            foreach (string recipient in recipients) {
-              Console.WriteLine(string.Format("Sending To:{1}{0}", delimiter, recipient));
-              ReportSet rs = reportSet;
-              //!!Add Security to report set Here
+						message.Subject = string.Format(AdHocSettings.EmailSubjectFormatString, reportName);
+						message.From = new MailAddress(AdHocSettings.EmailFromAddress);
 
-              MailMessage message = null;
-              SchedulerOutput sh = AdHocSettings.SchedulerOutputTypes[rs.SendEmailAs];
-              message = sh.GenerateMessage(rs);
-              message.Subject = string.Format(AdHocSettings.EmailSubjectFormatString, reportName);
-              message.From = new MailAddress(AdHocSettings.EmailFromAddress);
+						log.Write(string.Format("success{0}", delimiter));
+					}
 
-              try {
-                MailAddress addr = new MailAddress(recipient);
-                message.To.Add(addr);
-                SmtpClient client = new SmtpClient(AdHocSettings.SmtpServer);
-                client.Send(message);
-                Console.WriteLine(string.Format("success{0}", delimiter));
-              } catch (Exception ex) {
-                StringBuilder emessage = new StringBuilder();
-                while (ex != null) {
-                  emessage.Append(ex.Message + " ");
-                }
-                Console.WriteLine(emessage.ToString());
-                //message failed
-              }
-            }
-          } catch (Exception ex) {
-            StringBuilder emessage = new StringBuilder();
-            while (ex != null) {
-              emessage.Append(ex.Message + " ");
-            }
-            //messagefailed
-            Console.WriteLine(emessage.ToString());
-          }
-        }
-        if (!reportsFound) {
-          //No Reports Found
-          Console.WriteLine("No ReportsFound");
-        }
-      } catch { }
-      Console.ReadLine();
-    }
+					#endregion
 
-  }
-  public class CustomAdHocConfig : Izenda.AdHoc.FileSystemAdHocConfig {
-    public override void ConfigureSettings() {
-    }
+					log.Write(string.Format("Sending To:{0}", delimiter));
+					foreach (string recipient in recipients)
+						try 
+						{
+							log.Write(string.Format("{0} - ", recipient));
+							Utility.SendEmailEx(client, recipient, message, AdHocSettings.SmtpSecureConnection, AdHocSettings.SmtpPort);
+							log.Write(string.Format("success{0}", delimiter));
+						}
+						catch (Exception ex) 
+						{
+							StringBuilder emessage = new StringBuilder();
+							while (ex != null) 
+							{
+								emessage.Append(ex.Message + " ");
+								ex = ex.InnerException;
+							}
+							log.Write(string.Format("failure ({0}){1}", emessage, delimiter));
+						}
+					log.Write("Done.{0}", delimiter);
+				}
+				catch (Exception ex) 
+				{
+					StringBuilder emessage = new StringBuilder();
+					while (ex != null) 
+					{
+						emessage.Append(ex.Message + " ");
+						ex = ex.InnerException;
+					}
+					log.Write(string.Format("failure ({0}){1}", emessage, delimiter));
+				}
+			}
+			if (!reportsFound)
+				log.Write(string.Format("Reports Not Found {0}", delimiter));
+		}
+		catch { }
+		finally
+		{
+			AdHocContext.SetSchedulerExecuting(false);
+		}
+	}
 
-    public override ReportSet LoadReportSet(string reportName) {
-      ReportInfo reportInfo = new ReportInfo(reportName);
-      string fileName = Path.Combine(ReportPath, reportInfo.FullName + ".xml");
-      FileInfo file = new FileInfo(fileName);
-      if (!file.Exists) {
-        return null;
-      }
-      try {
-        StreamReader reader = file.OpenText();
-        string xml = reader.ReadToEnd();
-        reader.Close();
-        ReportSet reportSet = new ReportSet();
-        reportSet.ReadXml(xml);
-        reportSet.Global = reportInfo.TenantID == "_global_";
-        reportSet.ReportName = reportInfo.Name;
-        reportSet.ReportCategory = reportInfo.Category;
-        return reportSet;
-      } catch {
-        return null;
-      }
-    }
-
-    public override Izenda.AdHoc.ReportInfo[] ListReports() {
-      return base.ListReports();
-    }
-
-    public override void PreExecuteReportSet(Izenda.AdHoc.ReportSet reportSet) {
-    }
-  }
+	protected static string[] ListTenants()
+	{
+		if (string.IsNullOrEmpty(HttpContext.Current.Request.Params["tenants"]))
+			return new string[] { };
+		return HttpContext.Current.Request.Params["tenants"].Split(',');
+	}
 }
---
+++ Scheduler.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Scheduler : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+		ASP.global_asax.CustomAdHocConfig.InitializeReporting();
+		CustomScheduler.RunScheduledReports(Response.Output, DateTime.Now);
+		Response.Flush();
+		Response.End();
+    }
+}
